6a9bfc55c6ab53bb10af1bb72d830023aaf6be2a
Abhishek Math <41338114+mr-robot-abhi@users.noreply.github.com>
2024-06-22 23:10:27 +0530

    Update calculator.ts

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,5 @@
 export function add(numbers: string): number {
     if (numbers === "") return 0; // Implementation: Add Method for Empty String:
-    if (!numbers.includes(",")) return parseInt(numbers, 10); Implementation: Add Method for Single Number:
+    if (!numbers.includes(",")) return parseInt(numbers, 10); //Implementation: Add Method for Single Number:
     return -1; // Dummy return to compile
 }
