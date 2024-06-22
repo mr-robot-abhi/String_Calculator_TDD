07f999f486c7214c3276f85969c848299e3f1446
Abhishek Math <41338114+mr-robot-abhi@users.noreply.github.com>
2024-06-22 23:11:44 +0530

    Update calculator.test.ts

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -1,13 +1,17 @@
-// Test: Add Method Handles Empty String:
 import { add } from '../src/calculator';
-
+// Test: Add Method Handles Empty String:
 test('should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
+//Test: Add Method Handles Single Number:
 test('should return the number itself for a single number', () => {
     expect(add("1")).toBe(1);
 });
 //Test: Add Method Handles Two Numbers:
 test('should return the sum of two numbers', () => {
     expect(add("1,5")).toBe(6);
+});
+//Test: Add Method Handles Multiple Numbers:
+test('should return the sum of multiple numbers', () => {
+    expect(add("1,2,3,4")).toBe(10);
 });
