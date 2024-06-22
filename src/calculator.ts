080d34be0454352a131702af94cf97f1834a951e
Abhishek Math <41338114+mr-robot-abhi@users.noreply.github.com>
2024-06-22 23:11:11 +0530

    Update calculator.ts

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,8 @@
 export function add(numbers: string): number {
     if (numbers === "") return 0; // Implementation: Add Method for Empty String:
     if (!numbers.includes(",")) return parseInt(numbers, 10); //Implementation: Add Method for Single Number:
+    
+    const nums = numbers.split(",").map(n => parseInt(n, 10)); //Implementation: Add Method for Two Numbers:
+    return nums.reduce((sum, num) => sum + num, 0);
     return -1; // Dummy return to compile
 }
