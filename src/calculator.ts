50c4353a8d033de0bca5927c24588e37d3a7d9d1
Abhishek Math <41338114+mr-robot-abhi@users.noreply.github.com>
2024-07-01 21:59:01 +0530

    Update calculator.ts

--- src/calculator.ts
+++ src/calculator.ts
@@ -7,16 +7,15 @@ export function add(numbers: string): number {
         const delimiterEndIndex = numbers.indexOf("\n");
         delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
         numbers = numbers.substring(delimiterEndIndex + 1);
-    
+    }
+
     const nums = numbers.split(delimiter).map(n => parseInt(n, 10));
 
     //Implementation: Handle Negative Numbers:
-        
     const negativeNums = nums.filter(n => n < 0);
     if (negativeNums.length > 0) {
         throw new Error(`Negative numbers not allowed: ${negativeNums.join(",")}`);
     }
 
     return nums.reduce((sum, num) => sum + num, 0);
-    
 }
